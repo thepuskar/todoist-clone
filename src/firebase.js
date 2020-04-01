e0d8255c8614a2e07381cad0f7d809da3f7c57a6
devpuskar <puskar.ad@gmail.com>
2020-04-01 20:06:54 +0545

    Firebase and sass dependency installed and firebase api added

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDETxte7CMwM_PB8QyWAkRcgUEhvovBHOA",
+  authDomain: "todoapp-5c1aa.firebaseapp.com",
+  databaseURL: "https://todoapp-5c1aa.firebaseio.com",
+  projectId: "todoapp-5c1aa",
+  storageBucket: "todoapp-5c1aa.appspot.com",
+  messagingSenderId: "895407109955",
+  appId: "1:895407109955:web:656b0c16c61086222de1f6",
+  measurementId: "G-LP691GC0GK"
+};
+export { firebaseConfig as firebase };
