c488fe8ea2e0c7293ce21f4d17215f7a88e3ac20
devpuskar <puskar.ad@gmail.com>
2020-04-01 19:38:36 +0545

    Sidebar elements added

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,3 +1,40 @@
 import React from "react";
 
-export const Sidebar = () => <p>I am sidebar</p>;
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar
+} from "react-icons/fa";
+
+export const Sidebar = () => (
+  <div className="sidebar" data-testid="sidebar">
+    <ul className="sidebar_generic">
+      <li>
+        <span>
+          <FaInbox />
+        </span>
+        <span>Inbox</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendar />
+        </span>
+        <span>Today</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendarAlt />
+        </span>
+        <span>Next 7 days</span>
+      </li>
+    </ul>
+    <div className="sidebar-middle">
+      <span>
+        <FaChevronDown />
+      </span>
+      <h2>Projects</h2>
+    </div>
+    <ul className="sidebar-project">Projects Will be here</ul>
+  </div>
+);
