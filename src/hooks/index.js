3679d6ded6c8d4c126528d5dbddfbbc691167857
devpuskar <puskar.ad@gmail.com>
2020-04-04 19:32:09 +0545

    Agian hooks(useTasks and useProjects) added

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -4,9 +4,9 @@ import { firebase } from "../firebase";
 
 import { collectedTasksExist } from "../helpers";
 
-
 export const useTask = selectedProject => {
   const [tasks, setTasks] = useState([]);
+  const [archivedTasks, setArchivedTasks] = useState([]);
 
   useEffect(() => {
     let unsubscribe = firebase
@@ -26,5 +26,47 @@ export const useTask = selectedProject => {
         : selectedProject === "INBOX" || selectedProject === 0
         ? (unsubscribe = unsubscribe.where("date", "==", ""))
         : unsubscribe;
-  }, []);
+
+    unsubscribe = unsubscribe.onSnapshot(snapshot => {
+      const newTasks = snapshot.docs.map(task => ({
+        id: task.id,
+        ...task.data()
+      }));
+      setTasks(
+        selectedProject === "NEXT_7"
+          ? newTasks.filter(
+              task =>
+                moment(task.date, "DD-MM-YYYY").diff(moment(), "days") <= 7 &&
+                task.archived !== true
+            )
+          : newTasks.filter(task => task.archived !== true)
+      );
+
+      setArchivedTasks(newTasks.filter(task => task.archived !== false));
+    });
+    return () => unsubscribe();
+  }, [selectedProject]);
+  return { tasks, archivedTasks };
+};
+
+export const useProject = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    firebase.firestore
+      .collection("projects")
+      .where("userId", "==", "xD54Q3dGwp58SSim6ndf")
+      .orderBy("projectId")
+      .get()
+      .then(snapshot => {
+        const allProjects = snapshot.docs.map(projects => ({
+          ...projects.data(),
+          docId: projectId
+        }));
+        if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+          setProjects(allProjects);
+        }
+      });
+  }, [projects]);
+  return { projects, setProjects };
 };
